b3ca9bde69323aca9564b62f6c3fcce0eb61ddae
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-09 18:22:03 +0300

    edite routes in app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,29 @@
-import { Route, Routes } from "react-router-dom";
-import SharedLayout from "shared/sharedLayout";
-import Movies from "../pages/movies";
-import Home from "../pages/home";
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'shared/sharedLayout';
+import Movies from '../pages/movies';
+import Home from '../pages/home';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<SharedLayout/>}/>
-        <Route index element={<Home/>}/>
-        <Route path="movies" element={<Movies/>}/>
-      </Routes>
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 const Home = ()=>{
-    return <h1>Test</h1>
+    return <></>
 }
 
 export default Home;
