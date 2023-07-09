306fc39fc2e2d7ca718630ce67f34f7b1422f6a6
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-09 16:49:19 +0300

    added styles to the sharedlayout component

--- src/shared/sharedLayout/SharedLayout.jsx
+++ src/shared/sharedLayout/SharedLayout.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+import css from './SharedLayout.module.css';
 
- const SharedLayout = () => {
+const SharedLayout = () => {
   return (
     <>
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-      </nav>
-    </header>
-    <Outlet/>
+      <header>
+        <nav>
+          <Link className={css.link} to="/">
+            Home
+          </Link>
+          <Link className={css.link} to="/movies">
+            Movies
+          </Link>
+        </nav>
+      </header>
+      <Outlet />
     </>
   );
 };
