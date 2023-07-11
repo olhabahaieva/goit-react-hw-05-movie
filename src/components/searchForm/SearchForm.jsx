f54d4fa8284045ce8d88da347c08dfba312814ae
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-11 21:21:55 +0300

    removed useEffect() from searchForm

--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import MovieList from 'components/movieList/MovieList';
 import { getMovieBySearch } from 'shared/APIs/movieBySearch';
 
@@ -10,31 +10,24 @@ const SearchMovies = () => {
   // eslint-disable-next-line
   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-
-    const getMovies = async () => {
-      try {
-        setLoading(true);
-        setError(false);
-        const target = searchRef.current.value;
-        const fetchedMovies = await getMovieBySearch(target);
-        setMovies(fetchedMovies);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getMovies();
-
-    return () => controller.abort();
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const target = searchRef.current.value;
+    try {
+      setLoading(true);
+      setError(false);
+      const fetchedMovies = await getMovieBySearch(target);
+      setMovies(fetchedMovies);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <label htmlFor="search"></label>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="search">Search:</label>
       <input type="text" name="search" id="search" ref={searchRef} />
       <button type="submit">Submit</button>
       {movies.length > 0 && <MovieList movies={movies} />}
