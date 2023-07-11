16b4bf4b72db029e628855321d31d98ca77195d5
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-11 13:01:07 +0300

    moviebysearch API request edited

--- src/shared/APIs/movieBySearch.js
+++ src/shared/APIs/movieBySearch.js
@@ -4,11 +4,14 @@ const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'df510edfce68ed3b1f6165b74448260e';
 
 export async function getMovieBySearch(params){
-    await axios.request(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${params}`)
-    .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  try {
+    const controller = new AbortController();
+    const response = await axios.request(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${params}`,
+    { signal: controller.signal }
+    )
+    console.log(response.data.results);
+    return response.data.results;
+  } catch (error) {
+    
+  }
 }
