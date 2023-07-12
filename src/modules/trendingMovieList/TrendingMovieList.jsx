652358d7c5257eab3cfbe5fe458cde8ced635e3d
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-12 16:22:49 +0300

    added loading & error logic for TrendingMovieList

--- src/modules/trendingMovieList/TrendingMovieList.jsx
+++ src/modules/trendingMovieList/TrendingMovieList.jsx
@@ -35,6 +35,8 @@ const TrendingMovieList = () => {
   return (
     <div className={css.marginTop}>
       <h1 className={css.title}>Trending today</h1>
+      {error & <p>Sorry, something went wrong</p>}
+      {loading && <p>Data is loading</p>}
       {movies.length > 0 && <MovieList movies={movies} />}
     </div>
   );
