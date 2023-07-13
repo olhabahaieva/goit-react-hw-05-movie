366202d5e9bd6c6e776067cb51b2d0aee6a52c21
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-13 16:37:33 +0300

    added basic style to the MovieInfo component

--- src/components/movieInfo/MovieInfo.jsx
+++ src/components/movieInfo/MovieInfo.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import css from './MovieInfo.module.css';
 
 export const MovieInfo = ({ movie }) => {
   return (
-    <div>
-      <div>
-        <img
+    <div className={css.card}>
+    <div className={css.cardMain}>
+       <img
           src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
           alt={movie.title}
         />
+      <div>
+       
         <h1>{movie.title}</h1>
-        <p>{movie.vote_average}</p>
-        <p>Overview</p>
+        <p>User Score: {movie.vote_average}</p>
+        <p className={css.boldText}>Overview</p>
         <p>{movie.overview}</p>
-        <p>Genres</p>
+        <p className={css.boldText}>Genres</p>
         <p>{movie.genre_ids}</p>
       </div>
 
-      <div>
-        <p>Additional Information</p>
-        <Link to="/cast">Cast</Link>
-        <Link to="/reviews">Reviews</Link>
-      </div>
+      
     </div>
+    <div className={css.info}>
+      <div className={css.line}></div>
+    <p>Additional Information</p>
+    <Link to="/cast">Cast</Link>
+    <Link to="/reviews">Reviews</Link>
+    <div className={css.line}></div>
+  </div>
+  </div>
   );
 };
 
