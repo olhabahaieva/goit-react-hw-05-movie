f2933b42ab956ac6213c2cb053f9ea21b1801973
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-13 21:47:39 +0300

    cleaned up home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,6 @@
 import TrendingMovieList from 'modules/trendingMovieList/TrendingMovieList';
 // import MovieDetails from 'pages/movieDetails/MovieDetails';
 import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import getMovieDetails from 'shared/APIs/movieDetailsAPI';
 
 const Home = () => {
   return <TrendingMovieList />;
