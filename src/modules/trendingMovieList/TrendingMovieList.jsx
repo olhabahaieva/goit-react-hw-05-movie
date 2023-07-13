e426b7db7157a8be1cf6d340437ac1a60bb50a35
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-13 21:47:00 +0300

    moved logiv from home page to trending list

--- src/modules/trendingMovieList/TrendingMovieList.jsx
+++ src/modules/trendingMovieList/TrendingMovieList.jsx
@@ -2,14 +2,29 @@ import { getTrendingMovies } from 'shared/APIs/trendingMoviesAPI';
 import { useEffect, useState } from 'react';
 import MovieList from 'components/movieList/MovieList';
 import css from './TrendingMovieList'
+import MovieDetails from 'pages/movieDetails/MovieDetails';
+import getMovieDetails from 'shared/APIs/movieDetailsAPI';
+import { useParams } from 'react-router-dom';
 
 const TrendingMovieList = () => {
+  const { movieId } = useParams();
   // eslint-disable-next-line
   const [movies, setMovies] = useState([]);
   // eslint-disable-next-line
   const [loading, setLoading] = useState(false);
   // eslint-disable-next-line
   const [error, setError] = useState(false);
+
+  const [movie, setMovie] = useState();
+
+  const onClick = async (movieId) => {
+    try {
+      const fetchedMovie = await getMovieDetails(movieId);
+      setMovie(fetchedMovie);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const controller = new AbortController();
@@ -37,7 +52,8 @@ const TrendingMovieList = () => {
       <h1 className={css.title}>Trending today</h1>
       {error && <p>Sorry, something went wrong</p>}
       {loading && <p>Data is loading</p>}
-      {movies.length > 0 && <MovieList movies={movies} />}
+      {movies.length > 0 && <MovieList movies={movies} onClick={() => onClick(movieId)}/>}
+        {movie && <MovieDetails movie={movie} />}
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,11 @@
 import TrendingMovieList from 'modules/trendingMovieList/TrendingMovieList';
-import MovieDetails from 'pages/movieDetails/MovieDetails';
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import getMovieDetails from 'shared/APIs/movieDetailsAPI';
+// import MovieDetails from 'pages/movieDetails/MovieDetails';
+import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import getMovieDetails from 'shared/APIs/movieDetailsAPI';
 
 const Home = () => {
-  const [movie, setMovie] = useState();
-  const { movieId } = useParams();
-
-  const onClick = () => {
-    async function getMovie() {
-      try {
-        const fetchedMovie = await getMovieDetails(movieId);
-        setMovie(fetchedMovie);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getMovie();
-  };
-
-  return (
-    <>
-      <TrendingMovieList onClick={onClick}/>
-      {movie && <MovieDetails movie={movie} />}
-    </>
-  );
+  return <TrendingMovieList />;
 };
 
 export default Home;
