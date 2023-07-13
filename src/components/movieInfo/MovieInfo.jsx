625d05eb62fcfdd81bfa9558437c3e5e81566900
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-13 19:00:35 +0300

    clickable MovieCast from the link

--- src/components/movieInfo/MovieInfo.jsx
+++ src/components/movieInfo/MovieInfo.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import css from './MovieInfo.module.css';
+import MovieCast from 'components/movieCast/MovieCast';
 
 export const MovieInfo = ({ movie }) => {
+  const [showMovieCast, setShowMovieCast] = useState(false);
+
+  const handleShowMovieCast = () => {
+    setShowMovieCast((prevShowMovieCast) => !prevShowMovieCast);
+  };
+
   return (
     <div className={css.card}>
-    <div className={css.cardMain}>
-       <img
+      <div className={css.cardMain}>
+        <img
           src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
           alt={movie.title}
         />
-      <div>
-       
-        <h1>{movie.title}</h1>
-        <p>User Score: {movie.vote_average}</p>
-        <p className={css.boldText}>Overview</p>
-        <p>{movie.overview}</p>
-        <p className={css.boldText}>Genres</p>
-        {movie.genres.map(genre =>
-          <ul className={css.genres} key={genre.id}>
-            <li>{genre.name}</li>
-          </ul>)}
-        <p>{movie.genre_ids}</p>
+        <div>
+          <h1>{movie.title}</h1>
+          <p>User Score: {movie.vote_average}</p>
+          <p className={css.boldText}>Overview</p>
+          <p>{movie.overview}</p>
+          <p className={css.boldText}>Genres</p>
+          {movie.genres.map(genre => (
+            <ul className={css.genres} key={genre.id}>
+              <li>{genre.name}</li>
+            </ul>
+          ))}
+          <p>{movie.genre_ids}</p>
+        </div>
       </div>
-
-      
+      <div className={css.info}>
+        <div className={css.line}></div>
+        <p>Additional Information</p>
+        <Link onClick={handleShowMovieCast}>Cast</Link>
+        <Link to="/reviews">Reviews</Link>
+        <div className={css.line}></div>
+        {showMovieCast && <MovieCast movie={movie} />}
+      </div>
     </div>
-    <div className={css.info}>
-      <div className={css.line}></div>
-    <p>Additional Information</p>
-    <Link to="/cast">Cast</Link>
-    <Link to="/reviews">Reviews</Link>
-    <div className={css.line}></div>
-  </div>
-  </div>
   );
 };
 
