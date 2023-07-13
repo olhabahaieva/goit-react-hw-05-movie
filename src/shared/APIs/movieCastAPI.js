ae45aedf27cffbba34044d0bd37ea3b0003b284e
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-13 18:36:12 +0300

    added MovieCast API request

--- src/shared/APIs/movieCastAPI.js
+++ src/shared/APIs/movieCastAPI.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'df510edfce68ed3b1f6165b74448260e';
 
-async function getMovieDetails(movieId) {
+async function getMovieCast(movieId) {
   try {
     // const controller = new AbortController();
     const response = await axios.get(
-      `${BASE_URL}movie/${movieId}?api_key=${API_KEY}`
+      `${BASE_URL}movie/${movieId}/credits?api_key=${API_KEY}`
       // { signal: controller.signal }
     );
 
-    // console.log(response.data);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
@@ -19,4 +19,4 @@ async function getMovieDetails(movieId) {
   }
 }
 
-export default getMovieDetails;
+export default getMovieCast;
