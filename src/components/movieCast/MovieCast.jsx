7083c4d0ddccbbc4c7179aec5843d38151591760
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-13 19:23:47 +0300

    added MovieCastInfo component

--- src/components/movieCast/MovieCast.jsx
+++ src/components/movieCast/MovieCast.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import getMovieCast from 'shared/APIs/movieCastAPI';
+import MovieCastInfo from 'components/movieCastInfo/MovieCastInfo';
 
 export const MovieCast = () => {
-  return (
-    <div>MovieCast</div>
-  )
-}
+  const [movie, setMovie] = useState();
+  const { movieId } = useParams();
+//   console.log(movieId);
+
+  useEffect(() => {
+    async function getCast() {
+      try {
+        const fetchedMovies = await getMovieCast(movieId);
+        setMovie(fetchedMovies);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getCast();
+  }, [movieId]);
+
+  return <div>{movie && <MovieCastInfo movie={movie}/>}</div>;
+};
 
 export default MovieCast;
---
+++ src/components/movieCastInfo/MovieCastInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MovieCastInfo = () => {
+  return (
+    <div>MovieCastInfo</div>
+  )
+}
+
+export default MovieCastInfo;
