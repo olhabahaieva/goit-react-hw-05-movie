62ef03e09d6ef25b14e94eb9e3245686a6bcae54
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-14 14:19:57 +0300

    added missing comma to the MovieDetails API

--- src/shared/APIs/movieDetailsAPI.js
+++ src/shared/APIs/movieDetailsAPI.js
@@ -5,10 +5,10 @@ const API_KEY = 'df510edfce68ed3b1f6165b74448260e';
 
 async function getMovieDetails(movieId) {
   try {
-    // const controller = new AbortController();
+    const controller = new AbortController();
     const response = await axios.get(
-      `${BASE_URL}movie/${movieId}?api_key=${API_KEY}`
-      // { signal: controller.signal }
+      `${BASE_URL}movie/${movieId}?api_key=${API_KEY}`,
+      { signal: controller.signal }
     );
 
     // console.log(response.data);
