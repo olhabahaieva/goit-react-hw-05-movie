0058f4d4d532970e889c647db8c433f06879b7f6
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-14 18:45:02 +0300

    removed onClick - TrendingMovieList

--- src/modules/trendingMovieList/TrendingMovieList.jsx
+++ src/modules/trendingMovieList/TrendingMovieList.jsx
@@ -2,11 +2,8 @@ import { getTrendingMovies } from 'shared/APIs/trendingMoviesAPI';
 import { useEffect, useState } from 'react';
 import MovieList from 'components/movieList/MovieList';
 import css from './TrendingMovieList';
-import getMovieDetails from 'shared/APIs/movieDetailsAPI';
-import { useParams } from 'react-router-dom';
 
 const TrendingMovieList = () => {
-  const { movieId } = useParams();
   // eslint-disable-next-line
   const [movies, setMovies] = useState([]);
   // eslint-disable-next-line
@@ -16,15 +13,6 @@ const TrendingMovieList = () => {
   // eslint-disable-next-line
   const [movie, setMovie] = useState();
 
-  const onClick = async movieId => {
-    try {
-      const fetchedMovie = await getMovieDetails(movieId);
-      setMovie(fetchedMovie);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
     const controller = new AbortController();
     async function getMovies() {
@@ -33,7 +21,6 @@ const TrendingMovieList = () => {
         setError(false);
         const fetchedMovies = await getTrendingMovies(controller.signal);
         setMovies(fetchedMovies);
-        // console.log(fetchedMovies)
       } catch (error) {
         setError(true);
       } finally {
@@ -51,9 +38,7 @@ const TrendingMovieList = () => {
       <h1 className={css.title}>Trending today</h1>
       {error && <p>Sorry, something went wrong</p>}
       {loading && <p>Data is loading</p>}
-      {movies.length > 0 && (
-        <MovieList movies={movies} onClick={() => onClick(movieId)} />
-      )}
+      {movies.length > 0 && <MovieList movies={movies} />}
     </div>
   );
 };
