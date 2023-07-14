2656141a2664db5624f4bf99ced0b9cd15dfab74
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-14 19:05:49 +0300

    removed unused onCLick, state and API from MovieList

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,40 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import getMovieDetails from 'shared/APIs/movieDetailsAPI';
 
 const MovieList = ({ movies }) => {
-  const openRef = useRef(null);
-  // eslint-disable-next-line
-  const [movie, setMovie] = useState([]);
-  // eslint-disable-next-line
-  const [loading, setLoading] = useState(false);
-  // eslint-disable-next-line
-  const [error, setError] = useState(false);
-
   const filteredMovies = movies.filter(movie => movie.title);
-  const target = openRef.current;
-  const onClick = async => {
-    try {
-      setLoading(true);
-      setError(false);
-      const fetchedMovie = getMovieDetails(target);
-      setMovie(fetchedMovie);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   return (
     <ul>
-      {error && <p>Sorry, something went wrong</p>}
-      {loading && <p>Data is loading</p>}
+      {/* {error && <p>Sorry, something went wrong</p>}
+      {loading && <p>Data is loading</p>} */}
       {filteredMovies.map(movie => (
         <li key={movie.id}>
-          <Link onClick={onClick} to={`/movies/${movie.id}`}>
-            {movie.title}
-          </Link>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
         </li>
       ))}
     </ul>
