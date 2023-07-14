2c96d8b2253c92c40aa0793a884c834cb532c8f0
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-14 19:31:10 +0300

    removed unused error and loading JSX

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -6,8 +6,6 @@ const MovieList = ({ movies }) => {
 
   return (
     <ul>
-      {/* {error && <p>Sorry, something went wrong</p>}
-      {loading && <p>Data is loading</p>} */}
       {filteredMovies.map(movie => (
         <li key={movie.id}>
           <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
