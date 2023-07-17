3b8c511b3e99cdb96aa76d0cec0f65d57261b58f
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-17 21:14:19 +0300

    added default image

--- src/components/movieCastInfo/MovieCastInfo.jsx
+++ src/components/movieCastInfo/MovieCastInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import css from './MovieCastInfo.module.css';
+import myImage from 'images/myImage.jpg';
 
 export const MovieCastInfo = ({ movie }) => {
   const children = movie.map(el => (
     <li key={el.cast_id || el.id}>
       <img
-        src={`https://image.tmdb.org/t/p/w200/${el.profile_path}`}
+        src={el.profile_path ? `https://image.tmdb.org/t/p/w200/${el.profile_path}` : myImage}
         alt={el.name}
       />
       <p className={css.boldText}>{el.name}</p>
